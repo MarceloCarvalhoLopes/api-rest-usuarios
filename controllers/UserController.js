22230248643e83134c65ca9a4ee6a9b0e79dda7d
marcelocarvalholopes <marcelolcarvalho@gmail.com>
2023-01-10 06:51:27 -0300

    cadastro de usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 class UserController{
 
     async index(req, res){}
@@ -8,9 +10,20 @@ class UserController{
         
         if (email == undefined) {
             res.status(400);
-            res.json({err: "O e-mail é inválido!"})
+            res.json({err: "O e-mail é inválido!"});
+            return;
         }
         
+        var emailExists = await User.findEmail(email);
+        if (emailExists){
+            res.status(406);
+            res.json({err:"O e-mail já está cadastrado!"});
+            return;
+        }
+
+
+        await User.create(email,password,name);
+
         res.status(201);
         res.send("Tudo OK");
     }
